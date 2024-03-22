44a815373e4a5cc5dc04204a0f485f90e6324479
Will Pierson <willcpierson@gmail.com>
2024-03-22 18:42:11 -0400

    add new font, space links correctly

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -6,13 +6,17 @@ const NavBar = () => {
 
     return (
         <div id={styles.navBody}>
-            <h2 className={styles.navLinks}>Events</h2>
-            <h2 className={styles.navLinks}>Tournament Data</h2>
+            <div className={styles.sideLinks}>
+                <h2 className={styles.navLinks}>Events</h2>
+                <h2 className={styles.navLinks}>Tournament Data</h2>
+            </div>
             <div id={styles.navLogo}>
                 <img src={srgLogo} id={styles.navLogo} alt="" />
             </div>
-            <h2 className={styles.navLinks}>Staff</h2>
-            <h2 className={styles.navLinks}>Contact Us</h2>
+            <div className={styles.sideLinks}>
+                <h2 className={styles.navLinks}>Staff</h2>
+                <h2 className={styles.navLinks}>Contact Us</h2>
+            </div>
         </div>
     );
 };
