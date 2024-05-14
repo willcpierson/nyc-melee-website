65e3953706f420f87452ab6310cc4518f66df30c
Will Pierson <willcpierson@gmail.com>
2024-05-14 14:25:39 +0000

    clean up aboutPage, add stats

--- frontend/src/components/AboutPage/index.js
+++ frontend/src/components/AboutPage/index.js
@@ -1,7 +1,7 @@
 import styles from "./AboutPage.module.css";
 import SYGPatti from "../../pictures/aboutPagePics/SYGPatti.jpg";
+import NightclubStats from "../../pictures/aboutPagePics/NightclubStats.jpg";
 import SYGFinalComms from "../../pictures/aboutPagePics/SYGFinalComms.jpg";
-import SYGJoJo from "../../pictures/aboutPagePics/SYGJoJo.jpg";
 
 const AboutPage = () => {
 
@@ -10,30 +10,29 @@ const AboutPage = () => {
         <div id={styles.aboutBody}>
             <section className={styles.aboutDescription}>
                 <p className={styles.aboutParagraph}>NYC Melee is a non-profit organization with the aim of providing both a competitive and
-                    welcoming community environment, allowing players to build long-lasting friendships and
-                    unforgettable moments. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Aspernatur fugit libero deserunt accusantium maxime neque facilis repellat reprehenderit. 
-                    Tempore repellendus dolores deserunt hic enim soluta corporis, saepe inventore aliquam sint!
+                    inclusive community environment, allowing players to build long-lasting friendships and
+                    unforgettable moments. We're proud to bring top level talent to our grassroots scene,
+                    with our team consisting of professional level software engineers, photographers and videographers, event organizers,
+                    and more.
                 </p>
                 <img className={styles.aboutPics} src={SYGPatti} alt="" />
             </section>
             <section className={styles.aboutDescription}>
-                <img id={styles.middlePic} className={styles.aboutPics} src={SYGFinalComms} alt="" />
-                <p className={styles.aboutParagraph}>NYC Melee is a non-profit organization with the aim of providing both a competitive and
-                    welcoming community environment, allowing players to build long-lasting friendships and
-                    unforgettable moments. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Aspernatur fugit libero deserunt accusantium maxime neque facilis repellat reprehenderit. 
-                    Tempore repellendus dolores deserunt hic enim soluta corporis, saepe inventore aliquam sint!
+                <img id={styles.middlePic} className={styles.aboutPics} src={NightclubStats} alt="" />
+                <p className={styles.aboutParagraph}> With our weekly events averaging 86 entrants in 2023, we have solidified our flaship event, "The Nightclub",
+                as the biggest Super Smash Bros. Melee local in the world. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut iste commodi animi quasi
+                 perferendis in hic fugit ipsam velit excepturi perspiciatis nulla praesentium, molestias accusantium ut sequi consequuntur corrupti?
                 </p>
             </section>
             <section className={styles.aboutDescription}>
-                <p className={styles.aboutParagraph}>NYC Melee is a non-profit organization with the aim of providing both a competitive and
-                    welcoming community environment, allowing players to build long-lasting friendships and
-                    unforgettable moments. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                <p className={styles.aboutParagraph}> We've worked with numerous brands and sponsors in the past, including names such as
+                Redbull, Beastcoast, The Brooklyn Nets, and more. We're always looking to collaborate with more teams and communities in the future,
+                to not only run great events but newly define what it means to run an unforgettable event.
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Aspernatur fugit libero deserunt accusantium maxime neque facilis repellat reprehenderit. 
                     Tempore repellendus dolores deserunt hic enim soluta corporis, saepe inventore aliquam sint!
                 </p>
-                <img  className={styles.aboutPics} src={SYGJoJo} alt="" />
+                <img  className={styles.aboutPics} src={SYGFinalComms} alt="" />
             </section>
         </div>
     );
