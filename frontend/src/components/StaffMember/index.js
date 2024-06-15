6bea062aab18c2b82d2791e62e67b5fe3cbaf8cb
Will Pierson <willcpierson@gmail.com>
2024-06-15 15:37:22 +0000

    add Phil to staff data

--- frontend/src/components/StaffMember/index.js
+++ frontend/src/components/StaffMember/index.js
@@ -6,13 +6,23 @@ const StaffMember = () => {
 
 
     return (
-        <div id={styles.staffBox}>
+        <>
+            <div id={styles.staffBox}>
+                <div id={styles.textBox}>
+                    <h2 id={styles.nameTag}>Will Pierson</h2>
+                    <h3 id={styles.positionTitle}>President of the Board, Lead Tournament Organizer, Software Engineer</h3>
+                </div>
+                <img id={styles.portrait} src={willPortrait} alt="" />
+            </div>
+
+            <div id={styles.staffBox}>
             <div id={styles.textBox}>
-                <h2 id={styles.nameTag}>Will Pierson</h2>
-                <h3 id={styles.positionTitle}>President of the Board, Lead Tournament Organizer, Software Engineer</h3>
+                <h2 id={styles.nameTag}>Phil Nolan</h2>
+                <h3 id={styles.positionTitle}>Board Member, Head of Media & Marketing</h3>
             </div>
             <img id={styles.portrait} src={willPortrait} alt="" />
-        </div>
+            </div>
+        </>
     );
 };
 
